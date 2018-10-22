3ff52fecaca13fd09ca11f697b1da83a9ae4deea
Manjot Sidhu <manjot.techie@gmail.com>
2018-10-22 18:20:55 +0530

    PBRP: updates

--- app/src/main/java/com/github/pitchblackrecoveryproject/pitchblackrecoveryproject/MainActivity.java
+++ app/src/main/java/com/github/pitchblackrecoveryproject/pitchblackrecoveryproject/MainActivity.java
@@ -1,52 +1,76 @@
 package com.github.pitchblackrecoveryproject.pitchblackrecoveryproject;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //final static String path = Environment.getExternalStorageDirectory() + File.separator + "PBRP";
+    final static String path = "/sdcard/PBRP";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        logic();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    protected void logic() {
+        TextView message = (TextView) findViewById(R.id.info);
+        Process p;
+        try {
+            // Preform su to get root privileges
+            p = Runtime.getRuntime().exec("su");
+
+            // Attempt to write a file to a root-only
+            DataOutputStream os = new DataOutputStream(p.getOutputStream());
+
+            // Create folder if it doesn't exists
+            File pbrp = new File(path);
+            boolean success = true;
+            /*Log.i("PBRP", "Starting to create folder");
+            if (!pbrp.exists()) {
+                Log.i("PBRP", "e1");
+                success = pbrp.mkdir();
+                Log.i("PBRP", "e2");
             }
-        });
-    }
+            Log.i("PBRP", "Done");*/
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            // Copy default.prop/prop.default to PBRP
+            Log.i("PBRP", "Staring to write");
+            os.writeBytes("cp /prop.default " + path + "/build.info\n");
+            os.writeBytes("cp /default.prop " + path + "/build.info\n");
+            Log.i("PBRP", "Done writing");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // Check if file copied successfully
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // Close the terminal
+            os.writeBytes("exit\n");
+            os.flush();
+            try {
+                p.waitFor();
+                if (p.exitValue() != 255) {
+                    // TODO Code to run on success
+                    message.setText("root");
+                }
+                else {
+                    // TODO Code to run on unsuccessful
+                    message.setText("not root");
+                }
+            } catch (InterruptedException e) {
+                // TODO Code to run in interrupted exception
+                message.setText("not root");
+            }
+        } catch (IOException e) {
+            // TODO Code to run in input/output exception
+            message.setText("not root");
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
