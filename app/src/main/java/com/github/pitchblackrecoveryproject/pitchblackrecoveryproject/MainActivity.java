81a4be100ad20f370aa7f3fa6a0dce55bf98347a
Manjot Sidhu <manjot.techie@gmail.com>
2018-10-28 14:39:40 +0530

    Updates

--- app/src/main/java/com/github/pitchblackrecoveryproject/pitchblackrecoveryproject/MainActivity.java
+++ app/src/main/java/com/github/pitchblackrecoveryproject/pitchblackrecoveryproject/MainActivity.java
@@ -5,19 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Environment;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.simple.JSONObject;
@@ -26,11 +24,9 @@ import org.json.simple.parser.ParseException;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.commons.io.FileUtils;
 
 public class MainActivity extends AppCompatActivity {
     final static String path = Environment.getExternalStorageDirectory() + File.separator + "PBRP";
@@ -43,7 +39,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(getRootAccess((TextView) findViewById(R.id.info), new File(path + "/" + buildFile)) == 1)
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View e) {
+                init();
+            }
+        });
+
         init();
     }
 
@@ -55,27 +57,25 @@ public class MainActivity extends AppCompatActivity {
             new ReadTextTask().execute(url);
         }
         catch (MalformedURLException e) {
-            Log.e("PBRP", "ERROR CHECK LINE 56");
+            Log.e("PBRP", "ERROR MalformedURL Exception");
+            e.printStackTrace();
         }
     }
 
     protected void logic() {
         // Logic starts here
         TextView message = (TextView) findViewById(R.id.info);
-        message.setText(getString(R.string.info));
+        setMessage(getString(R.string.info));
         File pbrpInfo = new File(path + "/" + buildFile);
 
-        int root = getRootAccess(message, pbrpInfo);
         int result = -2;
 
-        if(root == 1) {
-            try {
-                result = checkUpdate(parseInfo(pbrpInfo), pbReleases);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            result = checkUpdate(parseInfo(pbrpInfo), pbReleases);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         switch(result) {
@@ -90,8 +90,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(URL... urls) {
             try {
+                setMessage(getString(R.string.info));
                 // Read all the text returned by the server
                 BufferedReader in = new BufferedReader(new InputStreamReader(urls[0].openStream()));
+                pbReleases = "";
                 String s = "";
 
                 while ((s = in.readLine()) != null) {
@@ -111,67 +113,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected int getRootAccess(TextView message, File pbrpInfo) {
-
-        Process p;
-        try {
-            // Preform su to get root privileges
-            p = Runtime.getRuntime().exec("su");
-
-            // Attempt to write a file to a root-only
-            DataOutputStream os = new DataOutputStream(p.getOutputStream());
-            File pbrp = new File(path);
-
-            // TODO Create folder if it doesn't exists
-            /*
-            boolean success = true;
-            Log.i("PBRP", "Starting to create folder");
-            if (!pbrp.exists()) {
-                Log.i("PBRP", "e1");
-                success = pbrp.mkdir();
-                Log.i("PBRP", "e2");
-            }
-            Log.i("PBRP", "Done");*/
-
-            // Copy default.prop/prop.default to PBRP
-            os.writeBytes("cp /prop.default " + path + "/" + buildFile + "\n");
-            os.writeBytes("cp /default.prop " + path + "/" + buildFile + "\n");
-
-            // Check if file successfully copied
-
-            if(!pbrpInfo.exists()) {
-                Log.e("PBRP", "Failed to copy " + buildFile + " file to PBRP folder");
-                message.setText("Failed to copy" + buildFile + "file to PBRP folder");
-            } else {
-                message.setText(buildFile + " copied to PBRP folder");
-            }
-
-            // Close the terminal
-            os.writeBytes("exit\n");
-            os.flush();
-            try {
-                p.waitFor();
-                if (p.exitValue() != 255) {
-                    // TODO Code to run on success
-                    return 1;
-                }
-                else {
-                    // TODO Code to run on unsuccessful
-                    message.setText("Device is not rooted.");
-                    return -1;
-                }
-            } catch (InterruptedException e) {
-                // TODO Code to run in interrupted exception
-                message.setText("Device is not rooted.");
-                return -1;
-            }
-        } catch (IOException e) {
-            // TODO Code to run in input/output exception
-            message.setText("Device is not rooted.");
-            return -1;
-        }
-    }
-
     public static String[] parseInfo(File s) {
         String[] result = new String[2];
 
@@ -188,16 +129,17 @@ public class MainActivity extends AppCompatActivity {
             br.close();
         }
         catch (IOException e) {
-            //You'll need to add proper error handling here
+            e.printStackTrace();
         }
 
-        Pattern ptn = Pattern.compile("ro.omni.version=[^\\n\\t-]+-(\\d+)-([^\\n\\t-]+)");
+        Pattern ptn = Pattern.compile("[^\\n\\t-]+-(\\d+)-([^\\n\\t-]+)");
         Matcher matchPtn = ptn.matcher(text);
 
         if(matchPtn.find()) {
             result[1] = matchPtn.group(1);
             result[0] = matchPtn.group(2);
         }
+
         Log.i("PBRP", Arrays.toString(result));
 
         return result;
@@ -208,8 +150,8 @@ public class MainActivity extends AppCompatActivity {
         JSONParser parser = new JSONParser();
         Reader reader = null;
 
-        Object jsonObj = null;
-        jsonObj = parser.parse(src);
+        Log.i("PBRP", src);
+        Object jsonObj = parser.parse(src);
 
         JSONObject release = (JSONObject) jsonObj;
 
@@ -242,5 +184,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void onUpdated(TextView message) {
         message.setText("Congrats! Your device is Up to Date.");
+    }
+
+    private void setMessage(String msg) {
+        TextView message = (TextView) findViewById(R.id.info);
+        message.setText(msg);
     }
 }
